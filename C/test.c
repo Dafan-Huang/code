52ac0069af4cb2f6573aff2998850d1931a874b5
2409630293 <2409630293@qq.com>
2023-04-01 10:50:01 +0800

    master

--- C/test.c
+++ C/test.c
@@ -1,35 +1,7 @@
 #include<stdio.h>
-void menu()
-{
-    printf("This is a menu\n");
-    choice();
-}
-void choice()
-{
-    int i;
-    scanf("%d",&i);
-    switch(i)
-    {
-        case 1:
-            printf("you choose 1\n");
-            system("pause");
-            break;
-        case 2:
-            printf("you choose 2\n");
-            system("pause");
-            break;
-        case 0://回到主菜单
-            menu();
-            break;
-        default:
-            menu();
-            break;
-    }
-    menu();
-}
 int main()
 {
-    menu();
-    choice();
+    int a;
+    scanf("Hello World %d", &a);
     return 0;
 }
