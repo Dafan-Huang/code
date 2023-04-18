c22481d4ccce79a4d76c56491ecfe879013b7722
2409630293 <2409630293@qq.com>
2023-04-18 14:47:59 +0800

    master

--- py/test.py
+++ py/test.py
@@ -1,4 +1,11 @@
-# 创建随机矩阵
+# 基于  的路径规划问题
+# 1. 从  到  的最短路径
+
 import numpy as np
-x=np.random.rand(10,1)'
-print(x)
+x,y,z = 0,0,0
+# 生成一个  的矩阵
+a = np.zeros((x,y,z))
+# 生成一个  的矩阵
+b = np.zeros((x,y,z))
+# 生成一个  的矩阵
+c = np.zeros((x,y,z))
