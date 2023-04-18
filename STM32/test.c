89428f8d3890ff2e7d44e8919c7642850f8dea6e
2409630293 <2409630293@qq.com>
2023-04-18 17:50:25 +0800

    master

--- STM32/test.c
+++ STM32/test.c
@@ -1,3 +1,23 @@
-//路径规划
+//动态数组
 #include<stdio.h>
-
+#include<stdlib.h>
+int main()
+{
+    int *p;//定义指针
+    int i;
+    int n;
+    printf("Please input the length of array:");
+    scanf("%d",&n);//输入数组长度
+    p=(int *)malloc(n*sizeof(int));//动态分配内存
+//  p=(int *)calloc(n,sizeof(int));
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&p[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",p[i]);
+    }
+    free(p);//释放内存
+    return 0;
+}
