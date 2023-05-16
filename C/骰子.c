a3b56ccb0e09231b0d8ea5bdae99079b8386fa9c
2409630293 <2409630293@qq.com>
2023-05-16 17:55:41 +0800

    master

--- C/骰子.c
+++ C/骰子.c
@@ -1,22 +1,27 @@
-//扔两颗骰子猜点数，扔5次，统计最终结果
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    int i, j, k, n, m, a[11] = { 0 };
-    srand(time(NULL));
-    for (i = 0; i < 5; i++)
-    {
-        n = rand() % 6 + 1;
-        m = rand() % 6 + 1;
-        k = n + m;
-        a[k - 2]++;
-    }
-    for (i = 0; i < 11; i++)
-    {
-        printf("%d点出现的次数为%d\n", i + 2, a[i]);
-    }
+    int dice, guess;
+    srand(time(NULL)); // 初始化随机种子
+
+    printf("猜测骰子点数（1-6），输入0结束游戏：\n");
+    do {
+        scanf("%d", &guess); // 输入猜测的点数
+        if (guess == 0) // 如果输入0，结束游戏
+            break;
+
+        dice = rand() % 6 + 1; // 随机生成骰子点数
+        printf("骰子点数为：%d\n", dice);
+
+        if (dice == guess)
+            printf("恭喜你猜对了！\n");
+        else
+            printf("很遗憾猜错了，再试一次吧。\n");
+    } while (1);
+
+    printf("游戏结束。\n");
     return 0;
 }
