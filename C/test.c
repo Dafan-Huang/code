55c946fe0ad35c0c6e1e0a02c647616e9c58c08d
2409630293 <2409630293@qq.com>
2023-06-13 12:32:27 +0800

    master

---
+++ C/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int k=0;
+    int i=(k=1);
+    printf("%d",i);
+    return 0;
+    // if (k=0)
+    // {
+    //     printf("Hello World\n");
+    // }
+    // else
+    // {
+    //     printf("error\n");
+    // }
+    // return 0;
+}
