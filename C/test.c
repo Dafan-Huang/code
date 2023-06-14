9264dc830fe3fcf90479b7c065a6b99433631928
2409630293 <2409630293@qq.com>
2023-06-14 13:21:39 +0800

    master

--- C/test.c
+++ C/test.c
@@ -1,17 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 int main()
 {
-    int k=0;
-    int i=(k=1);
-    printf("%d",i);
+    int dice1, dice2, sum;
+    
+    // 用当前时间初始化随机数生成器
+    srand(time(NULL));
+    
+    // 生成两个随机数，模拟扔两个骰子
+    dice1 = rand() % 6 + 1;
+    dice2 = rand() % 6 + 1;
+    
+    // 计算两个骰子的点数之和
+    sum = dice1 + dice2;
+    
+    // 显示结果
+    printf("first:%d\n", dice1);
+    printf("second:%d\n", dice2);
+    printf("sum is :%d\n", sum);
+    
     return 0;
-    // if (k=0)
-    // {
-    //     printf("Hello World\n");
-    // }
-    // else
-    // {
-    //     printf("error\n");
-    // }
-    // return 0;
 }
