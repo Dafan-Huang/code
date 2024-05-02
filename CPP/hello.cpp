8011b22f3c59926b9d93b096271e5b41065dfa7a
2409630293 <2409630293@qq.com>
2024-05-02 15:16:57 +0800

    master

--- CPP/hello.cpp
+++ CPP/hello.cpp
@@ -22,3 +22,46 @@ void printHelloWorld()
 {
     cout << "Hello World!" << endl;
 }
+
+// 绘制爱心
+void drawLove()
+{
+    cout << "    **    **" << endl;
+    cout << " *******" << endl;
+    cout << "  *****" << endl;
+    cout << "   ***" << endl;
+    cout << "    *" << endl;
+}
+
+```
+
+### 2.2 编译
+
+```shell
+g++ hello.cpp -o hello
+```
+
+### 2.3 运行
+
+```shell
+./hello
+```
+
+### 2.4 输出
+
+```shell
+Hello World!
+    **    **
+ *******
+  *****
+   ***
+    *
+    * 
+    * 
+    * 
+    * 
+    * 
+    * 
+    * 
+    * 
+    * 
