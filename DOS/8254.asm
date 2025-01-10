d3b1e5bf4af5a6e72a3a94e7021e20903e12b970
2409630293 <2409630293@qq.com>
2025-01-10 15:24:40 +0800

    更新8254定时器汇编实现，添加注释以说明各位置信号及其功能

--- DOS/8254.asm
+++ DOS/8254.asm
@@ -4,7 +4,10 @@ CODE SEGMENT  USE16
              ASSUME CS:CODE
     BEG:     JMP    START
     CCONPORT EQU    213H           ;控制口地址
-
+    ;D7D6 表示计数器选择
+    ;D5D4 表示读写方式选择
+    ;D3D2D1 表示工作方式选择
+    ;D0 表示数制选择
     CCONBIT1 EQU    00110110B      ;计数器0控制字
     CCONBIT2 EQU    01110110B      ;计数器1控制字
     CCONBIT3 EQU    10110110B      ;计数器2控制字
@@ -13,9 +16,10 @@ CODE SEGMENT  USE16
     CDPORT2  EQU    211H           ;计数器1初值端口
     CDPORT3  EQU    212H           ;计数器2初值端口
 
-    CHDBIT1  EQU    10000          ;计数器0初值
-    CHDBIT2  EQU    2000           ;计数器1初值
-    CHDBIT3  EQU    1000           ;计数器2初值
+    ;N=fclk/fout
+    CHDBIT1  EQU    400            ;计数器0初值      100KHz/400=250Hz
+    CHDBIT2  EQU    400            ;计数器1初值      10KHz/400=25Hz
+    CHDBIT3  EQU    400            ;计数器2初值      1KHz/400=2.5Hz
 
     START:   NOP                   ;启动延时
              MOV    DX,CCONPORT    ;写入控制字(计数器0)
@@ -46,3 +50,11 @@ CODE SEGMENT  USE16
              JMP    WT
 CODE ENDS
           END    BEG
+
+; (1): 关闭实验箱电源，按照下面原理图连线；
+; (2): 将8254的A0,A1               接至   扩展总线区A0，A1;
+; (3): 将8254的/RD,/WR             接至   扩展总线区/IOR,/IOW;
+; (4): 将8254的/CS                 接至   I/O地址区/210H;
+; (5): 将8254的OUT0,OUT1,OUT2      接至   LED显示区L7，L6，L5;
+; (6): 将8254的GATE0,GATE1,GATE2   接至   扩展实验区+5V;
+; (7): 将8254的CLK0,CLK1,CLK2      接至   时钟100K,10K,1K; 
