64fba59964732649f12c700411731a30612626e4
2409630293 <2409630293@qq.com>
2025-05-31 13:25:15 +0800

    新增背景颜色循环变化功能

--- .venv/Python/test.py
+++ .venv/Python/test.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import pyttsx3
+import itertools
 
 root = tk.Tk()
 root.title("Hello Window")
@@ -13,4 +14,12 @@ engine.say("你是大傻逼")
 engine.runAndWait()
 label.pack(expand=True)
 
+colors = ["red", "orange", "yellow", "green", "cyan", "blue", "purple"]
+color_cycle = itertools.cycle(colors)
+
+def change_bg():
+    root.configure(bg=next(color_cycle))
+    root.after(200, change_bg)
+
+change_bg()
 root.mainloop()
