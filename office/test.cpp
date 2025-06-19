a844266401e11c3a16330ea56ce0ec26abe0eebd
2409630293 <2409630293@qq.com>
2025-06-19 15:05:40 +0800

    feat: 删除 Office 类及相关文档管理功能，简化代码

--- office/test.cpp
+++ office/test.cpp
@@ -1,37 +1,6 @@
 #include <iostream>
-#include <string>
-#include <vector>
-
-// 模拟 Office 功能的类
-class Office {
-public:
-    Office(const std::string& name) : officeName(name) {}
-
-    void addDocument(const std::string& doc) {
-        documents.push_back(doc);
-        std::cout << "已添加文档: " << doc << std::endl;
-    }
-
-    void listDocuments() const {
-        std::cout << officeName << " 的文档列表:" << std::endl;
-        for (const auto& doc : documents) {
-            std::cout << "- " << doc << std::endl;
-        }
-    }
-
-private:
-    std::string officeName;
-    std::vector<std::string> documents;
-};
 
 int main() {
-    Office myOffice("测试Office");
-
-    myOffice.addDocument("报告.docx");
-    myOffice.addDocument("数据表.xlsx");
-    myOffice.addDocument("演示文稿.pptx");
-
-    myOffice.listDocuments();
-
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
