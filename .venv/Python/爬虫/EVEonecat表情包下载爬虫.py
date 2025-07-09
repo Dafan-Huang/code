fc3fefe22ace646c2ca44af1db6423452c4fda14
2409630293 <2409630293@qq.com>
2025-07-09 21:58:45 +0800

    feat: 优化EVEonecat表情包下载爬虫，添加下载进度条和错误处理

--- .venv/Python/爬虫/EVEonecat表情包下载爬虫.py
+++ .venv/Python/爬虫/EVEonecat表情包下载爬虫.py
@@ -1,39 +1,24 @@
+import os
 import requests
+from tqdm import tqdm
 
-# # 生成以 0001, 0002, 0003 为规律的数列
-# num_list = [f'{i:04d}' for i in range(1, 151)]
-
-# # 定义要下载的文件 URL 列表
-# for num in num_list:
-#     url_list=['http://motions.cat/gif/tmb/'num'.gif']
-
-# # 遍历 URL 列表，逐个下载文件
-# for url in url_list:
-#     # 发送 GET 请求，获取文件内容
-#     response = requests.get(url)
-
-#     # 获取文件名
-#     filename = url.split('/')[-1]
-
-#     # 保存文件到本地
-#     with open(filename, 'wb') as f:
-#         f.write(response.content)
+# 保存图片的文件夹
+save_dir = "images"
+os.makedirs(save_dir, exist_ok=True)
 
 # 生成以 0001, 0002, 0003 为规律的数列
 num_list = [f'{i:04d}' for i in range(1, 151)]
 
 # 定义要下载的文件 URL 列表
-# http://motions.cat/gif/nhn/0001.gif
 url_list = [f'http://motions.cat/gif/nhn/{num}.gif' for num in num_list]
 
 # 遍历 URL 列表，逐个下载文件
-for url in url_list:
-    # 发送 GET 请求，获取文件内容
-    response = requests.get(url)
-
-    # 获取文件名
-    filename = url.split('/')[-1]
-
-    # 保存文件到本地
-    with open(filename, 'wb') as f:
-        f.write(response.content)
+for url in tqdm(url_list, desc="Downloading"):
+    filename = os.path.join(save_dir, url.split('/')[-1])
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+    except requests.RequestException as e:
+        print(f"Failed to download {url}: {e}")
