c4d0f775c7397b2b8e31b1c83e2dd60b9c25cd74
2409630293 <2409630293@qq.com>
2026-08-09 17:57:59 +0800

    Remove unused file: 𠰷𠈱.txt from the JavaScript directory

--- Java/test.java
+++
@@ -1,8 +0,0 @@
-//hello world
-public class test {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
-这是一段没意义的话
-啦啦啦啦啦啦
--- Java/what.java
+++
@@ -1,9 +0,0 @@
-//random 1~6 rolling
-import java.util.Random;
-public class what {
-    public static void main(String[] args) {
-        Random random = new Random();
-        int num = random.nextInt(6)+1;
-        System.out.println(num);
-    }
-}
